ea1d91399aa0bab09de2151c01381e7cdf73897a
Idan Badin <Idanb@nayax.com>
2023-09-09 22:02:03 +0300

    first commit

---
+++ index.js
@@ -0,0 +1,431 @@
+
+receipt = {
+  "CustomerName": "עידן בדין",
+  "ID": "c03d6106-5b4f-4eb7-b61f-04a1ec1ebbba",
+  "OriginalText": null,
+  "Total": 232,
+  "TotalNoVat": 198.29,
+  "VatTotal": 33.71,
+  "Vat": 0,
+  "Currency": "ש\"ח",
+  "Discount": "58.00",
+  "Barcode": null,
+  "TransactionNumber": "110000428",
+  "PaymentTypeStr": null,
+  "PaymentType": 7,
+  "ReceiptType": 1,
+  "UploadedDate": null,
+  "CreatedDate": "2023-08-31T10:28:51.211Z",
+  "Action": 1,
+  "Target": "3",
+  "AdditionalTargets": [],
+  "POSId": null,
+  "BusinessID": "f419d1ba-355c-49c5-a756-9d989c6ab24d",
+  "BranchID": null,
+  "POSInternalId": null,
+  "tPOS": {
+    "InternalID": "1",
+    "Description": "1",
+    "IsOnline": false
+  },
+  "tBranch": {
+    "InternalID": "1",
+    "Latitude": null,
+    "Longitude": null,
+    "Language": 2,
+    "CompanyID": null,
+    "BranchName": "Foot Locker",
+    "BranchAddress": "דוד סחרוב 21 ראשל\"צ,  MALL הזהב",
+    "BranchAddressEng": null,
+    "BranchPhone": "03-3019121",
+    "Description": "",
+    "IsOnline": false,
+    "FranchiseeName": null,
+    "VatNumber": "516780640",
+    "SAPId": null
+  },
+  "tBusiness": null,
+  "IsFavorite": false,
+  "ExtraDiscreteField1": null,
+  "ExtraDiscreteField2": null,
+  "ExtraTextField1": null,
+  "ExtraTextField2": null,
+  "LoyaltyID": null,
+  "LoyalName": null,
+  "CashierName": "ויקה קושלניקוב",
+  "Items": [
+    {
+      "Name": "Air Jordan Zoom Air Comfort",
+      "Discount": null,
+      "Position": null,
+      "ItemCode": "3608",
+      "Price": 200,
+      "Quantity": 1,
+      "DiscountCalc": 0,
+      "Total": 200,
+      "SubItems": [],
+      "ParentId": null,
+      "AdditionalData": [
+        {
+          "Key": "BarCode",
+          "Value": "665415273038"
+        },
+        {
+          "Key": "Discount",
+          "Value": "58.00"
+        },
+        {
+          "Key": "PriceAfterDiscount",
+          "Value": "232.00"
+        },
+        {
+          "Key": "AmountAfterDiscount",
+          "Value": "232.00"
+        },
+        {
+          "Key": "discountPercent",
+          "Value": "20.00"
+        },
+        {
+          "Key": "FashionProp1",
+          "Value": "S~M"
+        },
+        {
+          "Key": "FashionProp2",
+          "Value": "Black"
+        },
+        {
+          "Key": "FashionProp3",
+          "Value": ""
+        },
+        {
+          "Key": "Promo1",
+          "Value": "232.00 ( 58.00)%20.00             "
+        }
+      ],
+      "ItemInfo": null,
+      "Categories": []
+    },
+    {
+      "Name": "Air Forece 1 Low Retro",
+      "Discount": null,
+      "Position": null,
+      "ItemCode": "3608",
+      "Price": 180,
+      "Quantity": 1,
+      "DiscountCalc": 0,
+      "Total": 180,
+      "SubItems": [],
+      "ParentId": null,
+      "AdditionalData": [
+        {
+          "Key": "BarCode",
+          "Value": "665415273038"
+        },
+        {
+          "Key": "Discount",
+          "Value": ""
+        },
+        {
+          "Key": "PriceAfterDiscount",
+          "Value": "232.00"
+        },
+        {
+          "Key": "AmountAfterDiscount",
+          "Value": "232.00"
+        },
+        {
+          "Key": "discountPercent",
+          "Value": "20.00"
+        },
+        {
+          "Key": "FashionProp1",
+          "Value": "S~M"
+        },
+        {
+          "Key": "FashionProp2",
+          "Value": "Black"
+        },
+        {
+          "Key": "FashionProp3",
+          "Value": ""
+        },
+        {
+          "Key": "Promo1",
+          "Value": "232.00 ( 58.00)%20.00             "
+        }
+      ],
+      "ItemInfo": null,
+      "Categories": []
+    },
+    {
+      "Name": "Nike Air Zoom Pegasus 39",
+      "Discount": null,
+      "Position": null,
+      "ItemCode": "3608",
+      "Price": 155,
+      "Quantity": 1,
+      "DiscountCalc": 0,
+      "Total": 155,
+      "SubItems": [],
+      "ParentId": null,
+      "AdditionalData": [
+        {
+          "Key": "BarCode",
+          "Value": "665415273038"
+        },
+        {
+          "Key": "Discount",
+          "Value": ""
+        },
+        {
+          "Key": "PriceAfterDiscount",
+          "Value": "232.00"
+        },
+        {
+          "Key": "AmountAfterDiscount",
+          "Value": "232.00"
+        },
+        {
+          "Key": "discountPercent",
+          "Value": "20.00"
+        },
+        {
+          "Key": "FashionProp1",
+          "Value": "S~M"
+        },
+        {
+          "Key": "FashionProp2",
+          "Value": "Black"
+        },
+        {
+          "Key": "FashionProp3",
+          "Value": ""
+        },
+        {
+          "Key": "Promo1",
+          "Value": "232.00 ( 58.00)%20.00             "
+        }
+      ],
+      "ItemInfo": null,
+      "Categories": []
+    },
+    {
+      "Name": "Air Jordan Zoom Air Comfort",
+      "Discount": null,
+      "Position": null,
+      "ItemCode": "3608",
+      "Price": 200,
+      "Quantity": 1,
+      "DiscountCalc": 0,
+      "Total": 200,
+      "SubItems": [],
+      "ParentId": null,
+      "AdditionalData": [
+        {
+          "Key": "BarCode",
+          "Value": "665415273038"
+        },
+        {
+          "Key": "Discount",
+          "Value": "58.00"
+        },
+        {
+          "Key": "PriceAfterDiscount",
+          "Value": "232.00"
+        },
+        {
+          "Key": "AmountAfterDiscount",
+          "Value": "232.00"
+        },
+        {
+          "Key": "discountPercent",
+          "Value": "20.00"
+        },
+        {
+          "Key": "FashionProp1",
+          "Value": "S~M"
+        },
+        {
+          "Key": "FashionProp2",
+          "Value": "Black"
+        },
+        {
+          "Key": "FashionProp3",
+          "Value": ""
+        },
+        {
+          "Key": "Promo1",
+          "Value": "232.00 ( 58.00)%20.00             "
+        }
+      ],
+      "ItemInfo": null,
+      "Categories": []
+    },
+    {
+      "Name": "Air Forece 1 Low Retro",
+      "Discount": null,
+      "Position": null,
+      "ItemCode": "3608",
+      "Price": 180,
+      "Quantity": 1,
+      "DiscountCalc": 0,
+      "Total": 180,
+      "SubItems": [],
+      "ParentId": null,
+      "AdditionalData": [
+        {
+          "Key": "BarCode",
+          "Value": "665415273038"
+        },
+        {
+          "Key": "Discount",
+          "Value": ""
+        },
+        {
+          "Key": "PriceAfterDiscount",
+          "Value": "232.00"
+        },
+        {
+          "Key": "AmountAfterDiscount",
+          "Value": "232.00"
+        },
+        {
+          "Key": "discountPercent",
+          "Value": "20.00"
+        },
+        {
+          "Key": "FashionProp1",
+          "Value": "S~M"
+        },
+        {
+          "Key": "FashionProp2",
+          "Value": "Black"
+        },
+        {
+          "Key": "FashionProp3",
+          "Value": ""
+        },
+        {
+          "Key": "Promo1",
+          "Value": "232.00 ( 58.00)%20.00             "
+        }
+      ],
+      "ItemInfo": null,
+      "Categories": []
+    },
+    {
+      "Name": "Nike Air Zoom Pegasus 39",
+      "Discount": null,
+      "Position": null,
+      "ItemCode": "3608",
+      "Price": 155,
+      "Quantity": 1,
+      "DiscountCalc": 0,
+      "Total": 155,
+      "SubItems": [],
+      "ParentId": null,
+      "AdditionalData": [
+        {
+          "Key": "BarCode",
+          "Value": "665415273038"
+        },
+        {
+          "Key": "Discount",
+          "Value": ""
+        },
+        {
+          "Key": "PriceAfterDiscount",
+          "Value": "232.00"
+        },
+        {
+          "Key": "AmountAfterDiscount",
+          "Value": "232.00"
+        },
+        {
+          "Key": "discountPercent",
+          "Value": "20.00"
+        },
+        {
+          "Key": "FashionProp1",
+          "Value": "S~M"
+        },
+        {
+          "Key": "FashionProp2",
+          "Value": "Black"
+        },
+        {
+          "Key": "FashionProp3",
+          "Value": ""
+        },
+        {
+          "Key": "Promo1",
+          "Value": "232.00 ( 58.00)%20.00             "
+        }
+      ],
+      "ItemInfo": null,
+      "Categories": []
+    }
+  ],
+  "AdditionalData": [
+    {
+      "Key": "SysPrnMemberId",
+      "Value": ""
+    },
+    {
+      "Key": "CustName",
+      "Value": ""
+    },
+    {
+      "Key": "CustCardCode",
+      "Value": ""
+    },
+    {
+      "Key": "NameDocumentOfPrint",
+      "Value": "חשבונית מס / קבלה מס'"
+    },
+    {
+      "Key": "TaxNumber",
+      "Value": "110000428"
+    },
+    {
+      "Key": "SaleNo",
+      "Value": "1189"
+    }
+  ],
+  "Payments": [
+    {
+      "Name": "מזומן",
+      "PaymentCode": "1",
+      "Amount": 232,
+      "PaymentInfo": "",
+      "LastDigits": null,
+      "UID": null,
+      "RRN": null,
+      "AdditionalData": null
+    }
+  ],
+  "MoneySaved": null,
+  "ItemsCount": 1,
+  "OrderID": null,
+  "SuppressSMS": false
+}
+
+function formatNumber(number) {
+  const formattedNumber = new Intl.NumberFormat('en-US', { style: 'currency', currency: 'USD' }).format(number);
+  return formattedNumber;
+}
+
+function getAddtionalValue(item, Key) {
+  return item.AdditionalData && item.AdditionalData.find(a=>a.Key == Key) ? item.AdditionalData.find(a=>a.Key == Key).Value : "bve";
+}
+
+for(var i=0; i < receipt.Items.length; i++) {
+  var item = receipt.Items[i];
+  var price = item.Price;
+  $("#itemsTbl").append("<tr class='spaceUnder' style='font-size: 16px'><td id='itemNameCell'>"+item.Name+"</td><td>"+formatNumber(price.toFixed(2))+"</td></tr>");
+  $("#itemNameCell").css("width", "250px");
+}
+
+$("#itemsTbl").append("<tr><td colspan='2' style='border-bottom: 2px solid black;padding-top: 20px'><td></tr>");
+$("#itemsTbl").append("<tr><td style='padding: 15px 0'>Total</td><td style='padding-top: 15px'>"+formatNumber(receipt.Total)+"</td></tr>");
